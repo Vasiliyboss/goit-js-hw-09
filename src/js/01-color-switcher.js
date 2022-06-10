29e166a7989237614c956d20d59038b9cae427af
Vasiliy <mazurboss2016@gmail.com>
2022-06-11 00:02:22 +0300

    fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
-
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let timerId = null;
+function changeColorBackground() {
+    btnStop.removeAttribute('disabled');
+    btnStart.setAttribute('disabled', true);
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+}
+btnStop.addEventListener('click', () => {
+    btnStart.removeAttribute('disabled');
+    btnStop.setAttribute('disabled', true);
+    clearInterval(timerId);
+    console.log("hello");
+});
+btnStart.addEventListener('click', changeColorBackground);
+btnStop.setAttribute('disabled', true);
